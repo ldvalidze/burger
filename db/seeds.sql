7403f2d0f37c11130684cabdb87a43baf5ddcc10
Levan <l.dvalidze@gmail.com>
2018-06-08 16:12:25 -0700

    schema and seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('peruvian steak burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('ahi tuna burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('grassfed burger', false);
