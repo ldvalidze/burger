c6bc87ce1ec73423e19621dc75f27a5810b7517c
Levan <l.dvalidze@gmail.com>
2018-06-12 18:38:32 -0700

    heroku DB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
 
 // Make connection.
 connection.connect(function(err) {
@@ -16,6 +10,19 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'burgers_db'
+  });
+}
 
 // Export connection for our ORM to use.
 module.exports = connection;
